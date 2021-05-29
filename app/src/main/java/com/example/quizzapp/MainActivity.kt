2c4b71773c74c774f69b348178fae73ea145bb68
Johnle <memetintin12345@gmail.com>
2021-05-29 12:54:20 +0700

    fix branch

---
+++ app/src/main/java/com/example/quizzapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.quizzapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.button);
+
+        btn.setOnClickListener {
+            val intent = Intent(this@MainActivity, Menu::class.java)
+            startActivity(intent)
+
+        }
+    }
+
+
+}
